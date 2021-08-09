ff0a2b4ec892980164fab18a54bb00fe6bcaa768
Justin Yue <jyue@salesforce.com>
2021-08-09 11:17:58 -0700

    add support to view

--- build/Readme.txt
+++ build/Readme.txt
@@ -4,3 +4,9 @@ How to
 sfdx force:package:create --name vlocity-extension --description "Soforce vlocity extension package" --packagetype Unlocked --path force-app --nonamespace --targetdevhubusername vdo20-hub
 2. Create new version
 sfdx force:package:version:create -p "vlocity-extension" -d force-app -f config/package-def.json --installationkeybypass --wait 120 -v vdo20-hub
+
+
+3. Bulk delete debug logs
+sfdx force:data:soql:query -q "Select id from ApexLog" -r csv > apexlog.csv  
+sfdx force:data:bulk:delete -s ApexLog -f apexlog.csv  
+
--- force-app/main/default/lwc/jsonAttributeViewer/jsonAttributeViewer.js
+++ force-app/main/default/lwc/jsonAttributeViewer/jsonAttributeViewer.js
@@ -1,6 +1,11 @@
 import { LightningElement, api, wire, track } from 'lwc';
 import { getRecord } from 'lightning/uiRecordApi';
+import LOCALE from '@salesforce/i18n/locale';
+import CURRENCY from '@salesforce/i18n/currency';
 import { updateRecord } from 'lightning/uiRecordApi';
+import getJSONAttribute from '@salesforce/apex/JSONAttributeSupportEx.getJSONAttribute';
+import getAttributeValues from '@salesforce/apex/JSONAttributeSupportEx.getAttributeValues';
+
 import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 
 const actions = [
@@ -57,91 +62,151 @@ export default class JsonAttributeViewer extends LightningElement {
         
     }
 
-    @wire(getRecord, { recordId: '$recordId', fields: '$jsonAttributeField' })
-    getJSONAttribute( { error, data }) {
+    @wire(getJSONAttribute, { recordId: '$recordId'})
+    getJSONAttributeEx( { error, data }) {
         if (error) {
-            // TODO
             console.log('Error');
-        } else if (data) {
-            // this.record = data;
-            this.jsonAttribute = data.fields.vlocity_cmt__JSONAttribute__c.value;
-            var attributesByCategory = JSON.parse(this.jsonAttribute);
-            //debugger;
-            this.attributes = [];
-            for (var categoryCode in attributesByCategory) {
-                var myAttributes = attributesByCategory[categoryCode];
-                for (var i = 0; i < myAttributes.length; i++) {
-                    var myAttribute = myAttributes[i];
+        } else {
+            if (data) {
+                console.log(data);
+                var metadata = data.metadata;
+                var values = data.values;
+                console.log(metadata);
+                console.log(values);
+                this.attributes = [];
+                for (const [key, myAttribute] of Object.entries(metadata.attributes)) {
+                    // console.log(`${key}: ${value}`);
 
                     var attribute = {
-                        name: myAttribute.attributedisplayname__c,
-                        code: myAttribute.attributeuniquecode__c,
-                        category_name: myAttribute.categoryname__c,
-                        category_code: myAttribute.categorycode__c,
-                        active: myAttribute.isactive__c,
-                        readonly: myAttribute.isreadonly__c,
+                        name: myAttribute.label,
+                        code: myAttribute.code,
+                        category_name: myAttribute.categoryName,
+                        category_code: myAttribute.categoryCode,
+                        // active: myAttribute.isactive__c,
+                        readonly: myAttribute.readonly,
                         text_color: 'slds-text-color_default',
                         is_changed: false,
-                        is_picklist: false,
-                        is_input: false,
-                        is_number: false,
-                        is_checkbox: false,
-                        is_combobox: false,
-                    };
-
-                    var myRuntimeInfo = myAttribute.attributeRunTimeInfo;
-                    attribute.data_type = myRuntimeInfo.dataType;
-
-                    attribute.value = myRuntimeInfo.value;
-                    attribute.display_value = attribute.value == null ? "" : attribute.value + ""; 
+                        is_picklist: myAttribute.valueType == 'picklist',
+                        is_input: myAttribute.valueType == 'text',
+                        is_date: myAttribute.valueType == 'date',
+                        is_datetime: myAttribute.valueType == 'datetime',
+                        is_number: myAttribute.valueType == 'number',
+                        is_currency: myAttribute.valueType == 'currency',
+                        is_percent: myAttribute.valueType == 'percent',
+                        is_checkbox: myAttribute.valueType == 'checkbox',
+                        value: values[key],
+                        display_value: values[key],
+                    };         
                     
-                    if (myRuntimeInfo.dataType === "Currency" ||
-                        myRuntimeInfo.dataType === "Percent" ||
-                        myRuntimeInfo.dataType === "Number") {
-                        attribute.input_type = 'number';
-                        attribute.is_number = true;
-                    } else if (myRuntimeInfo.dataType === "Text" || 
-                        myRuntimeInfo.dataType === "Lookup") {
-                        attribute.input_type = 'text';
-                        attribute.is_input = true;
-                    } else if (myRuntimeInfo.dataType === "Checkbox") {
-                        attribute.input_type = 'checkbox';                        
-                        attribute.is_checkbox = true;
-                    } else if (myRuntimeInfo.dataType === "Date") {
-                        attribute.input_type = 'date';
-                        attribute.is_input = true;
-                    } else if (myRuntimeInfo.dataType === "Datetime") {
-                        attribute.input_type = 'datetime';
-                        attribute.is_input = true;
-                    } else if (myRuntimeInfo.dataType === "Picklist") {
-                        attribute.is_combobox = true;
-
-                        attribute.value = Object.keys(myRuntimeInfo.selectedItem).length === 0 ? null : myRuntimeInfo.selectedItem.value;
-                        attribute.display_value = Object.keys(myRuntimeInfo.selectedItem).length === 0 ? null : myRuntimeInfo.selectedItem.displayText;
+                    console.log(attribute);
+                    if (attribute.is_picklist) {
                         attribute.options = [];
-                        for (var j = 0; j < myRuntimeInfo.values.length; j++) {
-                            attribute.options.push( {
-                                label: myRuntimeInfo.values[j].displayText,
-                                value: myRuntimeInfo.values[j].value
-                            });
-                        }
-                        
-                    } else if (myRuntimeInfo.dataType === "Multi Picklist") {
-                        attribute.input_type = 'text';
-                        attribute.is_input = true;
-                    } else {
-                        attribute.input_type = 'text';
-                        attribute.is_input = true;
-                    }
 
+                        myAttribute.values.forEach(item => {
+                            // console.log(item);
+                            attribute.options.push( {
+                                label: item.label,
+                                value: item.value
+                            });  
+
+                            if (item.value == attribute.value) {
+                                attribute.display_value = item.label;
+                            }
+                        });
+                    } 
+                    
                     this.attributes.push(attribute);
                 }
-
             }
 
-            console.log(this.attributes);
         }
     }
+
+    // @wire(getRecord, { recordId: '$recordId', fields: '$jsonAttributeField' })
+    // getJSONAttribute( { error, data }) {
+    //     if (error) {
+    //         // TODO
+    //         console.log('Error');
+    //     } else if (data) {
+    //         // this.record = data;
+    //         this.jsonAttribute = data.fields.vlocity_cmt__JSONAttribute__c.value;
+    //         var attributesByCategory = JSON.parse(this.jsonAttribute);
+    //         //debugger;
+    //         this.attributes = [];
+    //         for (var categoryCode in attributesByCategory) {
+    //             var myAttributes = attributesByCategory[categoryCode];
+    //             for (var i = 0; i < myAttributes.length; i++) {
+    //                 var myAttribute = myAttributes[i];
+
+    //                 var attribute = {
+    //                     name: myAttribute.attributedisplayname__c,
+    //                     code: myAttribute.attributeuniquecode__c,
+    //                     category_name: myAttribute.categoryname__c,
+    //                     category_code: myAttribute.categorycode__c,
+    //                     active: myAttribute.isactive__c,
+    //                     readonly: myAttribute.isreadonly__c,
+    //                     text_color: 'slds-text-color_default',
+    //                     is_changed: false,
+    //                     is_picklist: false,
+    //                     is_input: false,
+    //                     is_number: false,
+    //                     is_checkbox: false,
+    //                     is_combobox: false,
+    //                 };
+
+    //                 var myRuntimeInfo = myAttribute.attributeRunTimeInfo;
+    //                 attribute.data_type = myRuntimeInfo.dataType;
+
+    //                 attribute.value = myRuntimeInfo.value;
+    //                 attribute.display_value = attribute.value == null ? "" : attribute.value + ""; 
+                    
+    //                 if (myRuntimeInfo.dataType === "Currency" ||
+    //                     myRuntimeInfo.dataType === "Percent" ||
+    //                     myRuntimeInfo.dataType === "Number") {
+    //                     attribute.input_type = 'number';
+    //                     attribute.is_number = true;
+    //                 } else if (myRuntimeInfo.dataType === "Text" || 
+    //                     myRuntimeInfo.dataType === "Lookup") {
+    //                     attribute.input_type = 'text';
+    //                     attribute.is_input = true;
+    //                 } else if (myRuntimeInfo.dataType === "Checkbox") {
+    //                     attribute.input_type = 'checkbox';                        
+    //                     attribute.is_checkbox = true;
+    //                 } else if (myRuntimeInfo.dataType === "Date") {
+    //                     attribute.input_type = 'date';
+    //                     attribute.is_input = true;
+    //                 } else if (myRuntimeInfo.dataType === "Datetime") {
+    //                     attribute.input_type = 'datetime';
+    //                     attribute.is_input = true;
+    //                 } else if (myRuntimeInfo.dataType === "Picklist") {
+    //                     attribute.is_combobox = true;
+
+    //                     attribute.value = Object.keys(myRuntimeInfo.selectedItem).length === 0 ? null : myRuntimeInfo.selectedItem.value;
+    //                     attribute.display_value = Object.keys(myRuntimeInfo.selectedItem).length === 0 ? null : myRuntimeInfo.selectedItem.displayText;
+    //                     attribute.options = [];
+    //                     for (var j = 0; j < myRuntimeInfo.values.length; j++) {
+    //                         attribute.options.push( {
+    //                             label: myRuntimeInfo.values[j].displayText,
+    //                             value: myRuntimeInfo.values[j].value
+    //                         });
+    //                     }
+                        
+    //                 } else if (myRuntimeInfo.dataType === "Multi Picklist") {
+    //                     attribute.input_type = 'text';
+    //                     attribute.is_input = true;
+    //                 } else {
+    //                     attribute.input_type = 'text';
+    //                     attribute.is_input = true;
+    //                 }
+
+    //                 this.attributes.push(attribute);
+    //             }
+
+    //         }
+
+    //         console.log(this.attributes);
+    //     }
+    // }
 
     // get jsonAttribute() {
     //     if (this.record)
@@ -214,30 +279,49 @@ export default class JsonAttributeViewer extends LightningElement {
     }
 
     handleJSONAttributeRefreshClick(event) {
-        // Rebuild the this.attributes to force a datatable refresh
-        this.attributes = JSON.parse(JSON.stringify(this.attributes));
+        getAttributeValues({ recordId: this.recordId } )
+            .then((attribValues) => {
+                console.log(attribValues);
+                this.attributes.forEach(item => {
+                    item.value = attribValues[item.code];
+                    item.display_value = attribValues[item.code];
+                    item.text_color = 'slds-text-color_default';
+                });
+            })
+            .catch((error) => {
+                this.dispatchEvent(
+                    new ShowToastEvent({
+                        title: 'Error refreshing JSONAttribute',
+                        message: error.message,
+                        variant: 'error'
+                    })
+                );
 
-        var attributeVals = {};
-        var attributesByCategory = JSON.parse(this.jsonAttribute);
-        for (var categoryCode in attributesByCategory) {
-            var myAttributes = attributesByCategory[categoryCode];
-            for (var i = 0; i < myAttributes.length; i++) {
-                var myAttribute = myAttributes[i];
-                var myRuntimeInfo = myAttribute.attributeRunTimeInfo;
-                if (myRuntimeInfo.dataType === "Picklist") {
-                    attributeVals[myAttribute.attributeuniquecode__c] = Object.keys(myRuntimeInfo.selectedItem).length === 0 ? null : myRuntimeInfo.selectedItem.displayText;
-                } else {
-                    attributeVals[myAttribute.attributeuniquecode__c] = myRuntimeInfo.value;
-                }         
-            }
-        }
+            });
+        // // Rebuild the this.attributes to force a datatable refresh
+        // this.attributes = JSON.parse(JSON.stringify(this.attributes));
 
-        for (var i = 0; i < this.attributes.length; i++) {
-            this.attributes[i].value = attributeVals[this.attributes[i].code];
-            this.attributes[i].display_value = this.attributes[i].display_value;
-            this.attributes[i].is_changed = false;
-            this.attributes[i].text_color = 'slds-text-color_default';
-        }
+        // var attributeVals = {};
+        // var attributesByCategory = JSON.parse(this.jsonAttribute);
+        // for (var categoryCode in attributesByCategory) {
+        //     var myAttributes = attributesByCategory[categoryCode];
+        //     for (var i = 0; i < myAttributes.length; i++) {
+        //         var myAttribute = myAttributes[i];
+        //         var myRuntimeInfo = myAttribute.attributeRunTimeInfo;
+        //         if (myRuntimeInfo.dataType === "Picklist") {
+        //             attributeVals[myAttribute.attributeuniquecode__c] = Object.keys(myRuntimeInfo.selectedItem).length === 0 ? null : myRuntimeInfo.selectedItem.displayText;
+        //         } else {
+        //             attributeVals[myAttribute.attributeuniquecode__c] = myRuntimeInfo.value;
+        //         }         
+        //     }
+        // }
+
+        // for (var i = 0; i < this.attributes.length; i++) {
+        //     this.attributes[i].value = attributeVals[this.attributes[i].code];
+        //     this.attributes[i].display_value = this.attributes[i].display_value;
+        //     this.attributes[i].is_changed = false;
+        //     this.attributes[i].text_color = 'slds-text-color_default';
+        // }
     }
 
     handleJSONAttributeCopyClick(event) {
